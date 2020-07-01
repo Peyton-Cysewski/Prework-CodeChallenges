b0b06ca814f80c1934deab79f17f22d94b932299
Peyton-Cysewski <github.peyton.cysewski@gmail.com>
2020-07-01 15:08:17 -0700

    initialized files

---
+++ Code Challenges/Code Challenges/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Code_Challenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
