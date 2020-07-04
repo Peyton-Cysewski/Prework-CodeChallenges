d23e8b9552b3d50543a39ef43602947f4bfbc631
Peyton-Cysewski <github.peyton.cysewski@gmail.com>
2020-07-03 22:59:06 -0700

    Problem 3 completed

--- Code Challenges/Code Challenges/Program.cs
+++ Code Challenges/Code Challenges/Program.cs
@@ -6,13 +6,29 @@ namespace Code_Challenges
     {
         static void Main(string[] args)
         {
-            problem_2();
+            //problem_1();
+            //Console.Readline();
+            //Console.Clear();
+            //problem_2();
+            //Console.Clear();
+            //Console.Readline();
+
+            int[] test1 = new int[] { 1, 2, 3 };
+            int[] test2 = new int[] { 10, 2, 7 };
+            int[] test3 = new int[] { 2, 2 };
+            int[] test4 = new int[] { -1, 1, 0 };
+            problem_3(test1);
+            problem_3(test2);
+            problem_3(test3);
+            problem_3(test4);
+
+            //Console.Readline();
+            //Console.Clear();
+            //problem_4();
         }
 
         private static void problem_1()
         {
-            Console.Clear();
-
             int[] inputs = new int[5];
 
             for (int i = 0; i < 5; i++)
@@ -35,23 +51,44 @@ namespace Code_Challenges
 
         private static void problem_2()
         {
-            Console.Clear();
             Console.WriteLine("Enter a year to see if it is a leap year: ");
 
             int year = Convert.ToInt32(Console.ReadLine());
-            
+
             Console.Clear();
 
             if (year % 400 == 0)
             {
                 Console.WriteLine($"The year {year} is a leap year.");
-            } else if (year % 100 == 0)
+            }
+            else if (year % 100 == 0)
             {
                 Console.WriteLine($"The year {year} is not a leap year.");
-            } else if (year % 4 == 0)
+            }
+            else if (year % 4 == 0)
             {
                 Console.WriteLine($"The year {year} is a leap year.");
             }
+        }
+        private static void problem_3(int[] testArray)
+        {
+            int product = 1;
+            int sum = 0;
+            bool positive = true;
+
+            Console.Write("\nThe array: [ ");
+            foreach (int num in testArray)
+            {
+                if (num < 0)
+                {
+                    positive = false;
+                }
+                Console.Write($"{num} ");
+                product *= num;
+                sum += num;
+            }
+            Console.Write("] ");
+            Console.Write(sum == product && positive == true ? "contains a perfect sequence!" : "does not contain a perfect sequence.\n");
         }
     }
 }
